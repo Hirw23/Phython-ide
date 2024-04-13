7df7d78f3311cf9d217f273de8aafb020cd337f1
JOHN-KWIZERA <j.kwizera@alustudent.com>
2024-04-13 07:49:22 -0700

    review model

--- models.py
+++ models.py
@@ -5,3 +5,6 @@ LOGIN_USER = "SELECT * FROM users WHERE username = %s AND password = %s"
 # Product operations
 ADD_PRODUCT = "INSERT INTO products (product_name, description, price, seller_id) VALUES (%s, %s, %s, %s)"
 VIEW_PRODUCTS = "SELECT product_id, product_name, description, price, stock FROM products"
+
+# Review operations
+LEAVE_REVIEW = "INSERT INTO reviews (product_id, reviewer_id, review_text, rating) VALUES (%s, %s, %s, %s)"
